a3a7d0cc64ba66704ab8b8a1ab8f6cb437b4d8d8
Obinna Osuji <oosuji2017@kellogg.northwestern.edu>
2016-06-10 13:25:09 -0500

    End

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # Routes for the Musician resource:
   # CREATE
   get "/musicians/new", :controller => "musicians", :action => "new"
@@ -32,12 +34,13 @@ Rails.application.routes.draw do
   # DELETE
   get "/delete_album/:id", :controller => "albums", :action => "destroy"
   #------------------------------
+root 'musicians#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
